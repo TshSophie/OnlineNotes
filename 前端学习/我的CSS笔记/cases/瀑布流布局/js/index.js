78ad3b42f9a8878b42d7ffb91a6c2c4e8c5fe462
sophie <tansophie@qq.com>
2021-08-18 20:32:02 +0800

    新增笔记

---
+++ 前端学习/我的CSS笔记/cases/瀑布流布局/js/index.js
@@ -0,0 +1,41 @@
+;(function (params) {
+    var Waterfall = function (opt) {
+        this.el = document.getElementsByClassName(opt.el)[0];
+        this.oItems = this.el.getElementsByTagName('div');
+        this.columns = opt.columns;
+        this.gap = opt.gap;
+        this.itemWidth = (this.el.offsetWidth - (this.columns - 1) * this.gap) / this.columns;
+        this.heightArr = [];
+        this.init();
+    }
+    Waterfall.prototype.init = function () {
+        this.render()
+    }
+    Waterfall.prototype.render  =function () {
+        console.log(this.oItems);
+        var item = null, minIdx = -1;
+        for (let i = 0; i < this.oItems.length; i++) {
+            item = this.oItems[i];
+            item.style.width = this.itemWidth + 'px';
+            if(i < this.columns){
+                item.style.top = '0px';
+                item.style.left = i * (this.itemWidth + this.gap) + 'px';
+                this.heightArr.push(item.offsetHeight);
+            } else {
+                // 找出本行最低高度的项
+                var minIdx = getMinidx(this.heightArr);
+                item.style.left = this.oItems[minIdx].offsetLeft + 'px';
+                item.style.top = this.heightArr[minIdx] + this.gap + 'px';
+
+                // 更新该列的高度
+                this.heightArr[minIdx] += item.offsetHeight + this.gap;
+            }
+        }
+        function getMinidx(arr) {
+            return arr.indexOf(Math.min.apply(null, arr));
+        }
+    }
+
+    // 挂载到全局
+    window.Waterfall = Waterfall;
+})();
