bd776892def1f18b955b7f2c45ac90c3eea8d998
sophie <tansophie@qq.com>
2022-07-29 17:41:10 +0800

    八皇后问题求解代码

--- 数据结构与算法/计算之魂练习题/03从递推到递归/index.js
+++ 数据结构与算法/计算之魂练习题/03从递推到递归/index.js
@@ -6,31 +6,29 @@
 // 所以求n个台阶的走法变为求n-1和n-2个台阶的走法，则有f(n) = f(n - 1) + f(n - 2)(其中n > 2)。
 // 1 2 3 5 8 13 21 34 55 89
 function jumpStep(n) {
-  if(n <= 0) return -1;
-  if(n < 3) return n;
+  if (n <= 0) return -1;
+  if (n < 3) return n;
   return jumpStep(n - 1) + jumpStep(n - 2);
 }
 
 // 思路1-1 记忆化递归算法，将计算过的值缓存起来，减少重复计算
 function jumpStepImp(n, cache) {
-  if(cache[n]) return cache[n];
-  if(n <= 0) return -1;
-  if(n < 3) return cache[n] = n;
+  if (cache[n]) return cache[n];
+  if (n <= 0) return -1;
+  if (n < 3) return (cache[n] = n);
   cache[n] = jumpStep(n - 1) + jumpStep(n - 2);
-  return cache[n]
+  return cache[n];
 }
 
 // 思路2：至底向上，动态规划求解
 // 该问题当前状态只和之前的两个状态有关，因此可以采用动态规划，逐步解决问题
 // dp[i]=max(dp[i-1]+values[i], values[i])
 function jumpStepDp(n) {
-	if(n <= 0)
-		return 0;
+  if (n <= 0) return 0;
   let step = 0;
   let fStep = 1;
   let ffStep = 0;
-  for(let i = 1; i <= n; ++i)
-  {
+  for (let i = 1; i <= n; ++i) {
     step = fStep + ffStep;
     ffStep = fStep;
     fStep = step;
@@ -49,20 +47,73 @@ function jumpStepDp(n) {
 // 将起始柱上遗留的 1 个圆盘移动到目标柱上；
 // 将辅助柱上的所有圆盘移动到目标柱上。
 let num = 0;
-function moveTo(from, to) {
-  num++
-  console.log(`${num}: ${from} -> ${to}`);
+function moveTo(from, to, n) {
+  num++;
+  console.log(`第${num}躺: 第${n}个盘${from} -> ${to}`);
 }
 
-// 把n个盘子从from移动到to，用aux做中转
+// 把n个盘子从A移动到B，用aux做中转
 function Hanoi(A, B, aux, n) {
-  if(n < 2) {
-    return moveTo(A, B);
+  if (n < 2) {
+    return moveTo(A, B, n);
   }
   // 把n-1个盘子从A移动到aux，用B做中转
-  Hanoi(A, aux, B, n-1)
-  moveTo(A, B);
-  // 把n-1个盘子从aux移动到B，用B做中转
-  Hanoi(aux, B, A, n-1)
+  Hanoi(A, aux, B, n - 1);
+  moveTo(A, B, n);
+  // 把n-1个盘子从aux移动到B，用A做中转
+  Hanoi(aux, B, A, n - 1);
 }
-Hanoi('A', 'B', 'T', 5)
+// Hanoi("A", "B", "T", 3);
+
+/*****************************3、八皇后问题************************************** */
+// 根据题目条件，皇后不能同行，所以我们每行只能放置一个皇后。而刚好我们是8*8的棋盘，
+// 所以我们刚好是每行和每列都只能放置一个皇后。也就是说，我们一行一行地放置皇后。
+let max = 8; // 定义数组的最大长度
+let arr = []; // 声明数组
+let count = 0; // 记录解法的数量
+let resultArr = []
+// 判断方法
+function check(n) {
+  for (let i = 0; i < n; i++) {
+    // 判断n行和之前的n-1个皇后的是否可以摆放
+    // Math.abs(n-i)==Math.abs(arr[n]-arr[i]) 判断是否在同一直线上（成等腰三角形）
+    if (Math.abs(n - i) == Math.abs(arr[n] - arr[i]) || arr[n] == arr[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// 放置皇后
+function setQueen(n = 0) {
+  if (n == max) {
+    // 若摆放第9个皇后则结束调用
+    count++; // 方法+1
+    resultArr.push([...arr])
+    return;
+  }
+
+  // 从第1列摆放到第max列
+  for (let i = 0; i < max; i++) {
+    arr[n] = i; // 赋值给arr[n]，进一步判断
+    if (check(n)) {
+      setQueen(n + 1);
+    }
+  }
+}
+
+setQueen();
+console.log("八皇后问题解法：", count);
+// function queen(a, cur) {
+//   if (cur==a.length) { console.log(a); return; }
+//   for (var i = 0; i < a.length; i++) {
+//       a[cur] = i;
+//       var flag = true;
+//       for (var j = 0; j < cur; j++) {
+//           var ab = i - a[j];
+//           if (a[j]==i||(ab>0?ab:-ab)==cur-j) { flag=false; break };
+//       };
+//       if (flag) { queen(a,cur+1); }
+//   };
+// };
+// queen([1,1,1,1,1,1,1,1],0);
