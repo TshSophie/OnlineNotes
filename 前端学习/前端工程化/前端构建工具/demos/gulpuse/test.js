b58d6740e6a9edf6c1089cf2399ebc5854e116cd
sophie <tansophie@qq.com>
2023-01-09 17:53:06 +0800

    更新前端打包工具笔记

--- 前端学习/前端工程化/前端构建工具/demos/gulpuse/test.js
+++ 前端学习/前端工程化/前端构建工具/demos/gulpuse/test.js
@@ -13,7 +13,7 @@ const babel = require('gulp-babel')
 // 1-5. 导入 gulp-htmlmin 第三方, 用于压缩 html 文件
 const htmlmin = require('gulp-htmlmin')
 // 1-12. 导入 del 第三方, 用于删除文件夹
-// const del = require('del')
+const del = require('del')
 // 1-13. 导入 gulp-webserver 第三方, 用于在开发阶段启动服务器
 const webserver = require('gulp-webserver')
  
@@ -25,6 +25,14 @@ const cssHandler = () => {
     .pipe(autoprefixer())                 // 自动添加前缀
     .pipe(cssmin())                       // 进行 css 代码的压缩
     .pipe(gulp.dest('./dist/css/'))       // 放在指定目录内
+}
+//2.1 配置一个 打包plugin文件里css的任务
+const css2Handler = () => {
+  return gulp
+    .src('./src/plugin/*.css')               // 找到原始文件
+    .pipe(autoprefixer())                 // 自动添加前缀
+    .pipe(cssmin())                       // 进行 css 代码的压缩
+    .pipe(gulp.dest('./dist/plugin/'))       // 放在指定目录内
 }
 
 // 3. 配置一个 打包 less 文件的任务
@@ -48,10 +56,19 @@ const jsHandler = () => {
     .pipe(gulp.dest('./dist/js/'))
 }
 
+// 2.2 配置一个 打包plugin文件里js的任务
+const jsPluginHandler = () => {
+    return gulp
+    .src('./src/plugin/*.js')
+    .pipe(babel({ presets: [ '@babel/preset-env' ] })) // 把 ES6 转换成 ES5
+    .pipe(uglify()) // 压缩 js 文件
+    .pipe(gulp.dest('./dist/plugin/'))
+  }
+    
 // 5. 配置一个打包 html 文件的任务
 const htmlHandler = () => {
   return gulp
-    .src('./src/*.html')
+    .src('./src/views/*.html')
     .pipe(htmlmin({ // 进行压缩
       collapseWhitespace: true, // 把所有空格去掉
       collapseBooleanAttributes: true, // 简写所有布尔属性
@@ -63,20 +80,48 @@ const htmlHandler = () => {
       minifyCSS: true, // 压缩内嵌式 css 代码
       minifyJS: true, // 压缩内嵌式 js 代码
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/views/'))
 }
  
 // 6. 配置一个转移 img 文件的任务
 const imgHandler = () => {
   return gulp
-    .src('./src/images/**/*')
-    .pipe(gulp.dest('./dist/images/'))
+    .src('./src/img/**/*')
+    .pipe(gulp.dest('./dist/img/'))
+}
+ 
+// 7. 配置一个转移 video 文件的任务
+const videoHandler = () => {
+  return gulp
+    .src('./src/videos/**.*')
+    .pipe(gulp.dest('./dist/videos/'))
+}
+ 
+// 8. 配置一个转移 audio 文件的任务
+const audioHandler = () => {
+  return gulp
+    .src('./src/audios/**.*')
+    .pipe(gulp.dest('./dist/audios/'))
+}
+ 
+// 9. 配置一个转移 fonts 文件的任务
+const fontHandler = () => {
+  return gulp
+    .src('./src/fonts/**.*')
+    .pipe(gulp.dest('./dist/fonts/'))
+}
+ 
+// 10. 配置一个转移 data 文件的任务
+const dataHandler = () => {
+  return gulp
+    .src('./src/data/**.*')
+    .pipe(gulp.dest('./dist/data/'))
 }
  
 // 12. 配置一个删除任务
-// const delHandler = () => {
-//   return del(['./dist/'])
-// }
+const delHandler = () => {
+  return del(['./dist/'])
+}
  
 // 13. 配置一个启动服务器的任务
 const serverHandler = () => {
@@ -85,7 +130,7 @@ const serverHandler = () => {
     .pipe(webserver({ // 配置启动服务器
       host: 'localhost', // 域名
       port: '9888', // 端口号
-      open: './index.html', // 默认打开哪一个 html 文件, 你网站默认的首页
+      open: './views/index.html', // 默认打开哪一个 html 文件, 你网站默认的首页
       livereload: true, // 自动刷新页面, 当你的代码发生任何修改以后, 会自动刷新页面
       // 开始进行代理配置
       proxies: [
@@ -102,32 +147,39 @@ const serverHandler = () => {
 // 14. 配置一个监控任务
 const watchHandler = () => {
   gulp.watch('./src/js/*.js', jsHandler)
-  gulp.watch('./src/css/*.css', cssHandler)
+  // gulp.watch('./src/css/*.css', css2Handler)
   gulp.watch('./src/less/*.less', lessHandler)
-  gulp.watch('./src/*.html', htmlHandler)
-  gulp.watch('./src/images/*.html', imgHandler)
+  gulp.watch('./src/views/*.html', htmlHandler)
+  gulp.watch('./src/img/*.html', imgHandler)
 }
  
 // 11. 配置一个总任务
 const defaultHandler = gulp.series(
-//   delHandler,
-  gulp.parallel(
-    cssHandler,
-    lessHandler, 
-    jsHandler, 
-    htmlHandler, 
-    imgHandler),
-    serverHandler,
-    watchHandler
+  delHandler,
+  gulp.parallel(css2Handler,jsPluginHandler,lessHandler, jsHandler, htmlHandler, imgHandler),
+  serverHandler,
+  watchHandler
 )
-
+ 
 // end 导出你配置的任务
 module.exports = {
   lessHandler,
+  css2Handler,
+  jsPluginHandler,
   jsHandler,
   htmlHandler,
   imgHandler,
   default: defaultHandler,
-//   delHandler
+  delHandler
 }
  
+ 
+// // 假如我想做一个 打包 css 文件的配置
+// // gulp@3.x 的语法
+// gulp.tesk('cssHandler', function () {
+//   // 在这里面书写打包 css 文件的工作流程
+//   gulp
+//     .src('./src/css/*.css')
+//     .pipe('把css文件进行压缩转码')
+//     .pipe(gulp.dest('./dist/css/'))
+// })
